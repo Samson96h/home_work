71daffc5433e6855ee25d654526023a8d42d3f97
Samson96h <samsonhovhannisyan96@gmail.com>
2025-06-27 13:22:31 +0400

    Home work class metods

--- home.py
+++ home.py
@@ -24,38 +24,28 @@ def open_list (ml):
 print(open_list([1,2,3, [4,5,[0,0,0],6, [7,8,[40, 50, 60],9], 54, 6]]))
 
 
-def recursive_max(arr):
-    if len(arr) == 1:
-        return arr[0]
-    sub_max = recursive_max(arr[1:])
-    if arr[0] > sub_max:
-        return arr[0]
-    else:
-        return sub_max
+class MList:
+    def __init__(self, *args):
+        self.__my_list = [None] * len(args)
+        for i in range(len(args)):
+            self.__my_list[i] = args[i]
 
-print(recursive_max([4, 8, 96, 78, 56]))
+    @property
+    def list(self):
+        return self.__my_list
+
+    def __getitem__(self, index):
+        return self.__my_list[index]
+
+    def __setitem__(self, index, value):
+        self.__my_list[index] = value
+
+    def __repr__(self):
+        return str(self.list)
 
 
-def recursive_count(arr):
-    if not arr:
-        return 0
-    return 1 + recursive_count(arr[1:])
+m1 = MList(1,2,3,9)
+print(m1)
 
-print(recursive_count([4, 8, 96, 78, 56]))
-
-
-from random import randint
-
-def quick_sort(lst):
-    if len(lst) <= 1:
-        return lst
-    index = randint(0, len(lst) - 1)
-    pivot = lst[index]
-    rest = lst[:index] + lst[index + 1:]
-
-    less = [i for i in rest if i <= pivot]
-    greater = [i for i in rest if i > pivot]
-
-    return quick_sort(less) + [pivot] + quick_sort(greater)
-
-print(quick_sort([4, 8, 96, 78, 56]))
+m1[2] = 5
+print(m1)
